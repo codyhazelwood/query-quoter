ba4d911c92ae2a301cdfa817aaff646ebca1b233
Cody Hazelwood <codyhazelwood@gmail.com>
2017-02-09 22:03:48 -0600

    Make quoting and dequoting work (initial version)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,19 +5,36 @@ $(document).ready(function () {
     var btnDequote = $('button#dequote');
     var txtWithoutQuotes = $('textarea#textWithoutQuotes');
 
-
     //
     // Define logic
     //
 
-    function quote(text, quote, padding) {
-        return text;
+    function quote(text, quote, padding, leadingConcat) {
+        var lines = text.split(/\r\n|\n/g);
+
+        return lines
+            .map(function (line, idx) {
+                var first = idx === 0;
+                var last = idx === lines.length - 1;
+
+                var lcat = leadingConcat && !first ? '+ ' : '';
+                var tcat = !leadingConcat && !last ? ' +' : '';
+
+                return lcat +
+                    quote +
+                    line +
+                    (last ? '' : padding) +
+                    quote +
+                    tcat;
+            })
+            .join('\n');
     }
 
-    function dequote(text, quote, padding) {
-        return text;
+    function dequote(text) {
+        return text
+            .replace(/^(\+ ){0,1}["']/mg, '')
+            .replace(/(\\n| ){0,1}["']( \+){0,1}$/mg, '');
     }
-
 
     //
     // Setup click handlers
@@ -26,20 +43,19 @@ $(document).ready(function () {
     btnQuote.on('click', function (e) {
         e.preventDefault();
 
-        var quotes = '"';
-        var padding = '\\n';
+        var quotes = $('input[name=quoteRadios]:checked').val();
+        var padding = $('input[name=paddingRadios]:checked').val();
         var textToQuote = txtWithoutQuotes.val();
+        var leadingConcat = $('input[name=concatRadios]:checked').val() === 'true';
 
-        txtWithQuotes.val(quote(textToQuote, quotes, padding));
+        txtWithQuotes.val(quote(textToQuote, quotes, padding, leadingConcat));
     });
 
     btnDequote.on('click', function (e) {
         e.preventDefault();
 
-        var quotes = '"';
-        var padding = '\\n';
         var textToDequote = txtWithQuotes.val();
 
-        txtWithoutQuotes.val(dequote(textToDequote, quotes, padding));
+        txtWithoutQuotes.val(dequote(textToDequote));
     });
 });
